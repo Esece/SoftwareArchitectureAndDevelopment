27d647c8179074f603cc66b0eabfc5e2f8c39404
S78 <shigeru78@yahoo.com>
2018-01-19 19:29:58 -0600

    Create ArrayInitializerWithLengthAndValues.cs

---
+++ CodeSmells/ArrayInitializerWithLengthAndValues.cs
@@ -0,0 +1,7 @@
+// '3' is not necessary. When one more value is added, you will need to update the length to match.
+var numbers = new int[3] { 10, 20, 30 };
+
+// more maintanable ways are:
+var numbers = new int[] { 10, 20, 30 };
+var numbers = new[] { 10, 20, 30 };
+int[] numbers = { 10, 20, 30 };
